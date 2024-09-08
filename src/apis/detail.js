66a98417efff0eb6f4811e3e0724695560a5c05b
高兴 <14743651+gaoxi_ng@user.noreply.gitee.com>
2024-09-08 17:03:32 +0800

    详情热榜-基础组件封装和数据渲染

--- src/apis/detail.js
+++ src/apis/detail.js
@@ -8,3 +8,14 @@ export const getDetail = (id) => {
         }
     })
 }
+export const getHotGoodsAPI = ({ id, type, limit = 3 }) => {
+    return request({
+        url: 'https://pcapi-xiaotuxian-front-devtest.itheima.net/goods/hot',
+        params: {
+            id,
+            type,
+            limit
+        }
+
+    })
+}
