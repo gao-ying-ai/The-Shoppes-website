06f76385ab1a80f877c35144138bba09372830ad
高兴 <14743651+gaoxi_ng@user.noreply.gitee.com>
2024-09-09 15:31:14 +0800

    购物车删除功能

--- src/stores/cartStore.js
+++ src/stores/cartStore.js
@@ -19,9 +19,20 @@ export const useCartStore = defineStore('cart', () => {
             cartList.value.push(goods)
         }
     }
+
+    //删除购物车
+    const delCart = (skuId) => {
+        //splice
+        //filter
+        const idx = cartList.value.findIndex((item) => skuId === item.skuId)
+        cartList.value.splice(idx, 1)
+
+    }
+
     return {
         cartList,
-        addCart
+        addCart,
+        delCart
     }
 
 
