64b7e18f8bae07c9f39f883b2423aa1842903f1e
高兴 <14743651+gaoxi_ng@user.noreply.gitee.com>
2024-09-09 22:07:33 +0800

    合并购物车

--- src/utils/http.js
+++ src/utils/http.js
@@ -5,7 +5,7 @@ import 'element-plus/theme-chalk/el-message.css'
 import { useUserStore } from "@/stores/userStore"
 import router from "@/router"
 const httpInstance = axios.create({
-    baseURL: 'https://pcapi-xiaotuxian-front-devtest.itheima.net',
+    baseurl: 'https://pcapi-xiaotuxian-front-devtest.itheima.net',
     timeout: 5000
 })
 //拦截器
